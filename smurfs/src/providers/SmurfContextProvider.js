ee249855aef8be2a7d310de78395366e51f70c84
patrick-replogle <patrickr1138@gmail.com>
2019-11-23 09:21:20 -0800

    context, provider, and state change logic in place. now to add component structure and test if axios calls work.

--- smurfs/src/providers/SmurfContextProvider.js
+++ smurfs/src/providers/SmurfContextProvider.js
@@ -5,10 +5,70 @@ import { smurfContext } from "../contexts/smurfContext";
 
 const SmurfContextProvider = ({ children }) => {
   const [smurfList, setSmurfList] = useState([]);
-  const [isloading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
 
-  return <smurfContext.Provider>{children}</smurfContext.Provider>;
+  //fetch smurfs
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+        setSmurfList(res.data);
+      })
+      .catch(err => console.log("error fetching: ", err));
+    setIsLoading(false);
+  }, []);
+
+  //post smurf
+  const addSmurf = smurf => {
+    axios
+      .post("http://localhost:3333/smurfs", smurf)
+      .then(res => {
+        setSmurfList(res.data);
+      })
+      .catch(err => console.log("error posting: ", err));
+  };
+
+  //delete smurf
+
+  const deleteSmurf = smurfId => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${smurfId}`)
+      .then(res => {
+        setSmurfList(res.data);
+      })
+      .catch(err => console.log("error deleting: ", err));
+  };
+
+  //edit smurf
+
+  const editSmurf = (smurfId, smurf) => {
+    axios
+      .put(`http://localhost:3333/smurfs/${smurfId}`, smurf)
+      .then(res => {
+        setSmurfList(res.data);
+      })
+      .catch(err => console.log("error editing: ", err));
+    setIsEditing(false);
+  };
+
+  return (
+    <smurfContext.Provider
+      value={
+        (smurfList,
+        setSmurfList,
+        isLoading,
+        isEditing,
+        setIsEditing,
+        addSmurf,
+        deleteSmurf,
+        editSmurf)
+      }
+    >
+      {children}
+    </smurfContext.Provider>
+  );
 };
 
 export default SmurfContextProvider;

03b7d2532179aa6242e95569bad8a09aaf7e3817
patrick-replogle <patrickr1138@gmail.com>
2019-11-23 09:45:15 -0800

    Post request working. Smurf adding to page.

--- smurfs/src/components/Form.js
+++ smurfs/src/components/Form.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+
+import { smurfContext } from "../contexts/smurfContext";
 
 const Form = () => {
-  return <></>;
+  const { smurfList, addSmurf } = useContext(smurfContext);
+  const [formData, setFormData] = useState({ name: "", age: "", height: "" });
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addSmurf({ ...formData, id: smurfList.length });
+  };
+
+  return (
+    <>
+      <h2>Add a Smurf to the Village</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          name="name"
+          type="text"
+          placeholder="name"
+          value={formData.name}
+        />
+        <input
+          onChange={handleChange}
+          name="age"
+          type="number"
+          placeholder="age"
+          value={formData.age}
+        />
+        <input
+          onChange={handleChange}
+          name="height"
+          type="number"
+          placeholder="height"
+          value={formData.height}
+        />
+        <button>Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default Form;

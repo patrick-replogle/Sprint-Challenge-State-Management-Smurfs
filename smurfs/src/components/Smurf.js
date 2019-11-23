86271221efe9113099332357936c2f8a20d3f08a
patrick-replogle <patrickr1138@gmail.com>
2019-11-23 09:58:23 -0800

    Edit functionality working. Time to style.

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -6,14 +6,61 @@ const Smurf = props => {
   const { isEditing, setIsEditing, editSmurf, deleteSmurf } = useContext(
     smurfContext
   );
+
+  const [editFormData, setEditFormData] = useState({
+    name: props.smurf.name,
+    age: props.smurf.age,
+    height: props.smurf.height
+  });
+
+  const handleEditChange = e => {
+    setEditFormData({ ...editFormData, [e.target.name]: e.target.value });
+  };
+
+  const handleEditSubmit = e => {
+    e.preventDefault();
+    editSmurf(props.smurf.id, { ...editFormData, id: props.smurf.length });
+  };
+
   return (
-    <div className="smurfContainer">
-      <p>Name: {props.smurf.name}</p>
-      <p>Age: {props.smurf.age}</p>
-      <p>Height: {props.smurf.height}</p>
-      <button onClick={() => deleteSmurf(props.smurf.id)}>Delete</button>
-      <button onClick={() => setIsEditing(true)}>Edit</button>
-    </div>
+    <>
+      {isEditing ? (
+        <>
+          <form className="editForm" onSubmit={handleEditSubmit}>
+            <input
+              onChange={handleEditChange}
+              name="name"
+              type="text"
+              placeholder={props.smurf.name}
+              value={editFormData.name}
+            />
+            <input
+              onChange={handleEditChange}
+              name="age"
+              type="number"
+              placeholder={props.smurf.age}
+              value={editFormData.age}
+            />
+            <input
+              onChange={handleEditChange}
+              name="height"
+              type="number"
+              placeholder={props.smurf.height}
+              value={editFormData.height}
+            />
+            <button>Submit</button>
+          </form>
+        </>
+      ) : (
+        <div className="smurfContainer">
+          <p>Name: {props.smurf.name}</p>
+          <p>Age: {props.smurf.age}</p>
+          <p>Height: {props.smurf.height}</p>
+          <button onClick={() => deleteSmurf(props.smurf.id)}>Delete</button>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+        </div>
+      )}
+    </>
   );
 };
 
